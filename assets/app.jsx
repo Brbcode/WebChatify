078f1cfe6b77ae12811e2657b214ec8b0e035908
Bruno Garcia Tripoli <brbcode@gmail.com>
2023-04-03 20:55:12 +0200

    React | Material

--- assets/app.jsx
+++ assets/app.jsx
@@ -1,26 +1,21 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
 // start the Stimulus application
 import './bootstrap';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import router from './routes';
+import { themeWhite } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <React.Suspense fallback={<p>loading</p>}>
-      <RouterProvider router={router} />
-    </React.Suspense>
+    <ThemeProvider theme={themeWhite}>
+      <CssBaseline />
+      <React.Suspense fallback={<p>loading</p>}>
+        <RouterProvider router={router} />
+      </React.Suspense>
+    </ThemeProvider>
   </React.StrictMode>,
 );
