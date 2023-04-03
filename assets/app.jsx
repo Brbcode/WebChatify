4ba6b755bec258626fe2a7480ef03c91897ec01d
Bruno Garcia Tripoli <brbcode@gmail.com>
2023-04-03 19:55:53 +0200

    React | Base config

--- assets/app.jsx
+++ assets/app.jsx
@@ -10,3 +10,11 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>Hello World!!</h1>,
+);
