bfab0b9f36f885d6ccbbbd2359c6f969f65f2ae2
Bruno Garcia Tripoli <brbcode@gmail.com>
2023-04-03 20:55:12 +0200

    React | Material

--- assets/styles/theme.js
+++ assets/styles/theme.js
@@ -1,11 +1,25 @@
 import { createTheme } from '@mui/material';
 
-const themeWhite = createTheme();
+const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 960,
+    lg: 1280,
+    xl: 1920,
+  },
+};
+
+const baseTheme = createTheme({
+  breakpoints,
+});
+
+const themeWhite = createTheme({}, baseTheme);
 
 const themeDark = createTheme({
   palette: {
     mode: 'dark',
   },
-});
+}, baseTheme);
 
-export { themeWhite, themeDark };
+export { themeWhite, themeDark, baseTheme };
