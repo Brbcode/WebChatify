647ee95046303c771bd97e3718146344f1c2439b
Bruno Garcia Tripoli <brbcode@gmail.com>
2023-04-19 09:10:20 +0200

    GUI | SignUp to SignIn Transition

--- assets/routes/signroute.jsx
+++ assets/routes/signroute.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { Box } from '@mui/material';
 import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
 import ThemeSwitch from '../components/ThemeSwitch';
+import '../styles/transition/slide-fade.css';
 
 function SignRoute({ mode }) {
   const [modeState, setMode] = useState(mode);
   const [email, setEmail] = useState('');
+  const signUpRef = useRef(null);
+  const signInRef = useRef(null);
+  const nodeRef = modeState ? signUpRef : signInRef;
+  const [transitionClass, setTransitionClass] = useState('slide-fade-rev');
 
   return (
     <>
@@ -16,19 +23,43 @@ function SignRoute({ mode }) {
         right: 5,
       }}
       />
-      {
-        (modeState === 'up')
-          ? (
-            <SignUp
-              signInCallback={() => setMode('in')}
-              signUpCallback={(data) => {
-                setEmail(data.email);
-                setMode('in');
-              }}
-            />
-          )
-          : <SignIn signUpCallback={() => setMode('up')} defaultEmail={email} />
-      }
+      <Box sx={{ overflowX: 'hidden' }}>
+        <SwitchTransition>
+          <CSSTransition
+            key={modeState}
+            nodeRef={nodeRef}
+            timeout={500}
+            addEndListener={() => {
+              if (nodeRef.current.classList.contains(`${transitionClass}-enter`)) {
+                setTransitionClass((v) => (v === 'slide-fade' ? 'slide-fade-rev' : 'slide-fade'));
+              }
+            }}
+            classNames={transitionClass}
+            unmountOnExit
+          >
+            <Box ref={nodeRef}>
+              {
+                  (modeState === 'up')
+                    ? (
+                      <SignUp
+                        signInCallback={() => setMode('in')}
+                        signUpCallback={(data) => {
+                          setEmail(data.email);
+                          setMode('in');
+                        }}
+                      />
+                    )
+                    : (
+                      <SignIn
+                        signUpCallback={() => setMode('up')}
+                        defaultEmail={email}
+                      />
+                    )
+              }
+            </Box>
+          </CSSTransition>
+        </SwitchTransition>
+      </Box>
     </>
   );
 }
