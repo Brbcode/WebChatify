c5d04954026182d6044f3f1dbb4e0c90e09adcc5
Bruno Garcia Tripoli <brbcode@gmail.com>
2023-04-20 21:36:14 +0200

    GUI | App | SignGroup integration

--- assets/components/App.jsx
+++ assets/components/App.jsx
@@ -5,19 +5,28 @@ import { themeDark, themeWhite } from '../styles/theme';
 import AppLoad from '../routes/appload';
 import router from '../routes';
 import AlertPortal from './AlertPortal';
+import User from '../utils/User';
+import SignGroup from './SignGroup';
 
 function App() {
   const [theme, setTheme] = useState(themeWhite);
+  const [user, setUser] = useState(null);
 
   function onThemeChange({ detail }) {
     setTheme(detail.theme === 'dark' ? themeDark : themeWhite);
   }
+
+  const onSignIn = (data) => {
+    setUser(User.save(data));
+  };
 
   useEffect(() => {
     const sessionTheme = sessionStorage.getItem('theme');
     setTheme(sessionTheme === 'dark' ? themeDark : themeWhite);
 
     window.addEventListener('themeChanged', onThemeChange);
+
+    setUser(User.get());
 
     return () => {
       window.removeEventListener('themeChanged', onThemeChange);
@@ -30,7 +39,10 @@ function App() {
         <CssBaseline />
         <React.Suspense fallback={<AppLoad />}>
           <AlertPortal />
-          <RouterProvider router={router} />
+          {
+            user == null
+              ? <SignGroup signInCallback={onSignIn} /> : <RouterProvider router={router} />
+          }
         </React.Suspense>
       </ThemeProvider>
     </React.StrictMode>
---
+++ assets/utils/User.js
@@ -0,0 +1,39 @@
+const User = (() => {
+  let instance = null;
+
+  function create(email, displayName, roles, token) {
+    const user = {
+      email, displayName, roles, token,
+    };
+
+    return Object.freeze(user);
+  }
+
+  return {
+    save: ({
+      email, displayName, roles, token,
+    }) => {
+      sessionStorage.setItem('user', JSON.stringify({ email, displayName, roles }));
+      sessionStorage.setItem('token', token);
+
+      instance = create(email, displayName, roles, token);
+
+      return instance;
+    },
+    get: () => {
+      if (instance === null) {
+        const userItem = sessionStorage.getItem('user');
+        if (userItem === null) {
+          return null;
+        }
+        const { email, displayName, roles } = JSON.parse(userItem);
+        const token = sessionStorage.getItem('token');
+        instance = create(email, displayName, roles, token);
+      }
+
+      return instance;
+    },
+  };
+})();
+
+export default User;
