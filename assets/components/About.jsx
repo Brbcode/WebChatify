9dd11ca351d206f328374f1c32b31e52086c0843
Bruno Garcia Tripoli <brbcode@gmail.com>
2023-06-05 20:11:02 +0200

    About | bugfix | p nesting

--- assets/components/About.jsx
+++ assets/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Dialog, DialogContent, DialogContentText, DialogTitle, Link, Typography,
+  Dialog, DialogContent, DialogTitle, Link, Typography,
 } from '@mui/material';
 import PropTypes from 'prop-types';
 import { GitHub } from '@mui/icons-material';
@@ -10,27 +10,25 @@ function About({ onClose }) {
     <Dialog open onClose={onClose} fullWidth maxWidth="sm">
       <DialogTitle>About</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          <Typography variant="body" component="p">
-            WebChatify (version: alfa)
+        <Typography variant="body" component="p">
+          WebChatify (version: alfa)
+        </Typography>
+        <p>
+          Developed by:
+          <br />
+          <Typography variant="body" component="span" sx={{ opacity: 0.7 }}>
+            Bruno García Trípoli
           </Typography>
-          <p>
-            Developed by:
-            <br />
-            <Typography variant="body" component="span" sx={{ opacity: 0.7 }}>
-              Bruno García Trípoli
-            </Typography>
-          </p>
-          <p>
-            Open Source App:
+        </p>
+        <p>
+          Open Source App:
+          {' '}
+          <Link href="https://github.com/Brbcode/WebChatify" target="_blank" rel="noopener" sx={{ whiteSpace: 'nowrap' }}>
+            GitHub Repo
             {' '}
-            <Link href="https://github.com/Brbcode/WebChatify" target="_blank" rel="noopener" sx={{ whiteSpace: 'nowrap' }}>
-              GitHub Repo
-              {' '}
-              <GitHub sx={{ fontSize: 15 }} />
-            </Link>
-          </p>
-        </DialogContentText>
+            <GitHub sx={{ fontSize: 15 }} />
+          </Link>
+        </p>
       </DialogContent>
     </Dialog>
   );
